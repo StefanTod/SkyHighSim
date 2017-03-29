a3f87b0aea85f457f3dfe83a356985a0639fb97a
Vesnonoka,Anna A <a.vesnenok@student.fontys.nl>
2017-03-29 16:03:21 +0200

    Flight Class added

---
+++ Airtraffic_Simulator/Flight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airtraffic_Simulator
+{
+    class Flight
+    {
+        private String id;
+        private Airport departudeAirport;
+        private Airport destinationAirport;
+        private TimeSpan estimatedDuration;
+        private DateTime departureTime;
+        private DateTime arrivalTime;
+    }
+}
