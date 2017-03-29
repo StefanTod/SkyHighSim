c1c86c0fee5f3b1a49b0732602308c03decfa55f
Kyurchiev,Andrea A.G <a.kyurchiev@student.fontys.nl>
2017-03-29 15:37:06 +0200

    Problem and Airplane classes added

---
+++ Airtraffic_Simulator/Airplane.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airtraffic_Simulator
+{
+    class Airplane
+    {
+        private string id;
+        private int capacity;
+        private double speed;
+        private double fuel;
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+        }
+        public double Speed
+        {
+            get
+            {
+                return speed;
+            }
+        }
+        public double Fuel
+        {
+            get
+            {
+                return fuel;
+            }
+        }
+        public void Update()
+        {
+
+        }
+        public void UpdateMovement()
+        {
+
+        }
+    }
+}
