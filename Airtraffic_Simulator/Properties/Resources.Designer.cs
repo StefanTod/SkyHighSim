8581db4eeaebf56909c8720b688dc99e268d5e8b
Vesnonoka,Anna A <a.vesnenok@student.fontys.nl>
2017-03-29 19:00:38 +0200

    Map adding

--- Airtraffic_Simulator/Properties/Resources.Designer.cs
+++ Airtraffic_Simulator/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Airtraffic_Simulator.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap europemap {
+            get {
+                object obj = ResourceManager.GetObject("europemap", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap plane {
             get {
                 object obj = ResourceManager.GetObject("plane", resourceCulture);
