fa342335da099d10ef92211bcced3ac4e7013896
Hristo <hristokgj@gmail.com>
2017-03-29 16:19:43 +0200

    Added Network class and Regions enumerator

---
+++ Airtraffic_Simulator/Regions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airtraffic_Simulator
+{
+    public enum Regions
+    {
+        EUROPE,
+        AFRICA,
+        NORTHAMERICA
+    }
+}
