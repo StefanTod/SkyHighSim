d18084354bd44d080d05fc6bb92863274141ee20
Hristo <hristokgj@gmail.com>
2017-03-31 14:31:53 +0200

    Added calculations for UpdateMovement() of airplanes

--- Airtraffic_Simulator/Airplane.cs
+++ Airtraffic_Simulator/Airplane.cs
@@ -15,7 +15,7 @@ namespace Airtraffic_Simulator
         private double fuel;
         public Status status = Status.LANDED;
         private int counterTicks = 0;
-        private Flight flight;
+        public Flight flight;
         public Point CurrentLocation { get; set; }
         public Bitmap Image { get; set; }
         public string Id
@@ -80,8 +80,41 @@ namespace Airtraffic_Simulator
         }
         public void UpdateMovement()
         {
+            int x1 = this.flight.DepartureAirport.Location.X;
+            int x2 = this.flight.DestinationAirport.Location.X;
+            int y1 = this.flight.DepartureAirport.Location.Y;
+            int y2 = this.flight.DestinationAirport.Location.Y;
+
+            int xCurrent = this.CurrentLocation.X;
+            int yCurrent = this.CurrentLocation.Y;
             // calculations of movement
-        }
+            double currentdistance = Math.Sqrt(Math.Pow(xCurrent - x1, 2) + Math.Pow(yCurrent - y1, 2));
+            double distancepassed = (20 * 30 / 60); // Distance = speed * time  --> speed = 20px/hour time = 20 minutes 
+            int A = (y1 - y2);
+            int B = (x1 - x2);
+            if(B!=0 && A!=0) // calculate new point based on distance passed and current location
+            {
+                double slope = A / B;
+                double k = distancepassed / Math.Sqrt(1 + Math.Pow(slope, 2));
+                int xNew = (int)Math.Round(xCurrent + k * 1);
+                int yNew = (int)Math.Round(yCurrent + k * slope);
+                this.CurrentLocation = new Point(xNew, yNew);
+            }
+            else if (A==0) //then y stays the same, add distance to x
+            {
+                int xNew = (int)Math.Round(distancepassed) + xCurrent;
+                int yNew = yCurrent;
+                this.CurrentLocation = new Point(xNew, yNew);
+            }
+            else //x stays the same, add distance to y
+            {
+                int xNew = xCurrent;
+                int yNew = (int)Math.Round(distancepassed) + yCurrent;
+                this.CurrentLocation = new Point(xNew, yNew);
+            }
+
+           
+         }
         public Airplane(string id,int capacity,double speed, double fuel)
         {
             this.capacity=capacity;
