9f1a2f9883d50313009db0d28eb2f4fd6f658bb5
Hristo <hristokgj@gmail.com>
2017-05-29 19:10:19 +0200

    Added ChangePriority Method in Queue
    ChangePriority changes the priority of an element in the queue by keeping the Heap property (WORKS ONLY WHEN INCREASING)

--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -21,20 +21,74 @@ namespace UnitTests
             q.AddPlaneToQueueTest(15, plane3);
 
             Assert.AreEqual(plane3, q.pQueue.PeekValue());
-            Assert.AreEqual(plane3,q.DequeueHighestPriority());
+        }
+        [TestMethod]
+        public void TestQueueRemoval()
+        {
+            Airplane plane1 = new AirplanePassenger("1", 1, 1, 1, new PointF(5, 5));
+            Airplane plane2 = new AirplanePassenger("2", 1, 1, 1, new PointF(5, 5));
+            Airplane plane3 = new AirplanePassenger("3", 1, 1, 1, new PointF(5, 5));
+            Queue q = new Queue(null);
+            q.AddPlaneToQueueTest(5, plane1);
+            q.AddPlaneToQueueTest(10, plane2);
+            q.AddPlaneToQueueTest(15, plane3);
+            Assert.AreEqual(plane3, q.DequeueHighestPriority());
+        }
+        [TestMethod]
+        public void TestQueueRemovalAndPeek()
+        {
+            Airplane plane1 = new AirplanePassenger("1", 1, 1, 1, new PointF(5, 5));
+            Airplane plane2 = new AirplanePassenger("2", 1, 1, 1, new PointF(5, 5));
+            Airplane plane3 = new AirplanePassenger("3", 1, 1, 1, new PointF(5, 5));
+            Queue q = new Queue(null);
+            q.AddPlaneToQueueTest(5, plane1);
+            q.AddPlaneToQueueTest(10, plane2);
+            q.AddPlaneToQueueTest(15, plane3);
+            Assert.AreEqual(plane3, q.DequeueHighestPriority());
             Assert.AreEqual(plane2, q.pQueue.PeekValue());
+        }
+        [TestMethod]
+        public void TestPriorityChange()
+        {
+            Airplane plane1 = new AirplanePassenger("1", 1, 1, 1, new PointF(5, 5));
+            Airplane plane2 = new AirplanePassenger("2", 1, 1, 1, new PointF(5, 5));
+            Airplane plane3 = new AirplanePassenger("3", 1, 1, 1, new PointF(5, 5));
+            Queue q = new Queue(null);
+            q.AddPlaneToQueueTest(5, plane1);
+            q.AddPlaneToQueueTest(10, plane2);
+            q.AddPlaneToQueueTest(15, plane3);
+            q.ChangePriority(20, plane1); // plane 1 now has highest priority
+            Assert.AreEqual(plane1, q.pQueue.PeekValue());
+        }
+        [TestMethod]
+        public void CombinedTest()
+        {
+            Airplane plane1 = new AirplanePassenger("1", 1, 1, 1, new PointF(5, 5));
+            Airplane plane2 = new AirplanePassenger("2", 1, 1, 1, new PointF(5, 5));
+            Airplane plane3 = new AirplanePassenger("3", 1, 1, 1, new PointF(5, 5));
+            Queue q = new Queue(null);
+            q.AddPlaneToQueueTest(5, plane1);
+            q.AddPlaneToQueueTest(10, plane2);
+            q.AddPlaneToQueueTest(15, plane3);
+            Assert.AreEqual(plane3,q.DequeueHighestPriority());
+            q.ChangePriority(12, plane1);
+            q.ChangePriority(7, plane2);
+            Assert.AreEqual(plane1, q.pQueue.PeekValue());
         }
         [TestMethod]
         public void TestPriorityPointsCalculation()
         {
-            Airplane plane1 = new AirplanePassenger("1",200,3,5,new PointF(5,5));
-            plane1.AddFlight(new FlightPassenger("1", new Airport("asd", 5, new PointF(5, 5), 5), new Airport("asd", 5, new PointF(5, 5), 5), TimeSpan.MaxValue, DateTime.Now, DateTime.Now, 199));
+            PointF testPoint = new PointF(5, 5);
+            Airport testAirport = new Airport("asd", 5, testPoint, 5);
 
-            Airplane plane2 = new AirplaneCargo("2",50,3,8, new PointF(5, 5));
-            plane2.AddFlight(new FlightCargo("2", new Airport("asd",5,new PointF(5,5),5),new Airport("asd",5,new PointF(5,5),5), TimeSpan.MaxValue, DateTime.Now, DateTime.Now, 25));
+            Airplane plane1 = new AirplanePassenger("1",200,3,5,testPoint);
+            plane1.AddFlight(new FlightPassenger("1", testAirport,testAirport, TimeSpan.MaxValue, DateTime.Now, DateTime.Now, 199));
 
-            Airplane plane3 = new AirplanePassenger("3", 100, 3, 11, new PointF(5, 5));
-            plane3.AddFlight(new FlightPassenger("3", new Airport("asd", 5, new PointF(5, 5), 5), new Airport("asd", 5, new PointF(5, 5), 5), TimeSpan.MaxValue, DateTime.Now, DateTime.Now, 25));
+            Airplane plane2 = new AirplaneCargo("2",50,3,8,testPoint);
+            plane2.AddFlight(new FlightCargo("2", testAirport,testAirport, TimeSpan.MaxValue, DateTime.Now, DateTime.Now, 25));
+
+            Airplane plane3 = new AirplanePassenger("3", 100, 3, 11,testPoint);
+            plane3.AddFlight(new FlightPassenger("3",testAirport,testAirport, TimeSpan.MaxValue, DateTime.Now, DateTime.Now, 25));
 
             Queue q = new Queue(null);
             q.AddPlaneToQueue(plane1);
